4ff2b757645b3ec16793d3e9937d021ce99d1a19
Arvids Rozitis <arvids.rozitis.cits@gmail.com>
2023-02-01 01:46:43 +0200

    made posible to upload post to the server

--- CRUD/src/App.tsx
+++ CRUD/src/App.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import uuid from 'react-uuid'
 import { useState, useEffect } from 'react'
 import './App.css'
 import Loading from './components/Loading'
@@ -12,58 +13,101 @@ interface Task {
 
 
 function App() {
-  const [tasks, setTasks] = useState<Task[] | null>(null)
-
   useEffect(() => {
     axios.get<Task[]>('http://localhost:3004/tasks').then(({data}) => {
       setTasks(data)
       console.table(data)
     })
   }, []);//[] only one time runs on page load
+  const [tasks, setTasks] = useState<Task[] | null>(null);
+  const [inputValue, setInputValue] = useState<string>('')
+
+  const addTaskHandler = () => {
+    console.log('kosmos')
+    const newTask = {
+      id: uuid(),
+      description: inputValue,
+      isDone: false
+    }
+    axios.post<Task>(`http://localhost:3004/tasks`, newTask).then(() => {
+      console.log('notika'); 
+      setInputValue('')
+      axios.get<Task[]>('http://localhost:3004/tasks').then(({data}) => {
+        setTasks(data)
+      })
+    });
+  }
+
+
+
+ 
 
   
   
   return (
     <div className="App">
       {tasks ? <TaskList tasks={tasks} />:<Loading />}
+      
      
-      <AddTask />
-    </div>
-  )
-  
-}
-
-export default App
-export type { Task }
-function AddTask() {
-  return (
-    <div className='addTask__wrapper'>
-      <form className='addTask__form'>
+      <form className='addTask__form' onSubmit={(e) => {
+           e.preventDefault()
+        }}>
         <h3 className='addTask__heading'>add new task</h3>
-        <input className='addTask__input'/>
+        <input
+          type="text"
+          className='addTask__input'
+          placeholder='taks description'
+          value={inputValue}
+          onChange={(e)=> {             
+            e.preventDefault()
+            console.log(inputValue)
+            setInputValue(e.target.value)
+          }}
+        />
         <button
           className='button button__addTask'
-          type='submit'
+          onClick={addTaskHandler}
         >
           add
         </button>
       </form>
+
+
     </div>
   )
 }
 
+export default App
+export type { Task }
+
+// function AddTask() {
+//     return (
+//         <form className='addTask__form' onSubmit={(e) => {
+//            e.preventDefault()
+//         }}>
+//           <h3 className='addTask__heading'>add new task</h3>
+//           <input
+//             type="text"
+            
+//             className='addTask__input'
+//             placeholder='taks title'
+//             value={inputValue}
+//             onChange={(e)=> {             
+//               e.preventDefault()
+//               console.log(inputValue)
+//               setInputValue(e.target.value)
+//             }}
+//           />
+//           <button
+//             className='button button__addTask'
+//           >
+//             add
+//           </button>
+//         </form>
+//     )
+//   }
 
 
-//  function TaskList({tasks}):Task[] {
-//   tasks.map((task:Task) => {
-//      return (
-//        <div>
-//          <p>{task.description}</p>
-//        </div>
-//      )
-//    }) 
-
-//  }
 
 
 
--- CRUD/src/components/TaskList.tsx
+++ CRUD/src/components/TaskList.tsx
@@ -1,13 +1,14 @@
+import uuid from 'react-uuid';
 import { Task } from "../App";
 
 const TaskList = ({tasks}: any ) => {  
     return (
         <div className="todo__list">
             {tasks?.map((task:Task) =>(
-            <div className='task__item'>{task.description}</div>                
+            <div className='task__item' key={task.id}>{task.description}</div>                
             ))}
         </div>
-) 
+    ) 
 }
 
 export default TaskList;
