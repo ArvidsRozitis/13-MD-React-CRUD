1b1016af3c5144083587bf6837403e0615c4a1be
Arvids Rozitis <arvids.rozitis.cits@gmail.com>
2023-02-02 11:36:12 +0200

    Cleared some code

--- CRUD/src/components/TaskList/TaskItem/TaskEditForm/TaskEditForm.tsx
+++ CRUD/src/components/TaskList/TaskItem/TaskEditForm/TaskEditForm.tsx
@@ -65,10 +65,3 @@ export const TaskEditForm = ({ taskId, taskTitle, taskDescription, setTasks, ind
   };
 
 export default TaskEditForm
-
-const UpdateTaskButton = () => {
-  return (
-    <>
-    </>
-  )
-}
